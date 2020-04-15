392b23f567427c467917345fada65c57439e6f0d
James Chapman <chapmanjamesg@gmail.com>
2020-04-15 11:01:18 -0500

    created my first dialog box

--- HelloError.java
+++ HelloError.java
@@ -1,5 +1,8 @@
 public class HelloError{
     public static void main(String[] args){
+        //this is how you comment out one line
+        /*this is how you 
+        comment out multiple lines */
         System.out.println("Hello");
         System.out.println("This is a test");
     }
